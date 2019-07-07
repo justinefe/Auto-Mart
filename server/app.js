d153e6957b3c1f9b02a3af0d123cd633edf50d40
Justin <efejustin3@gmail.com>
2019-07-07 22:48:18 +0100

    chore(Set up chore): installs cors[finishes#167123204]

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import yaml from 'yamljs';
 import bodyParser from 'body-parser';
@@ -8,6 +9,7 @@ import route from './router/route';
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const swaggerdoc = yaml.load(`${__dirname}/../swagger.yaml`);
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerdoc));
